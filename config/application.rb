d14e75b65c0a0a8806f355d578e6699613cab159
hase-n323 <159225788+hase-n323@users.noreply.github.com>
2024-07-22 01:30:53 +0900

    デプロイ2 top変更

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,13 @@ module RailsNew
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    # ここから下5行を追記
+    config.generators do |g|
+      g.helper false             # helper ファイルを作成しない
+      g.test_framework false     # test ファイルを作成しない
+      g.skip_routes true         # ルーティングの記述を作成しない
+    end
+	  # ここまで追記
   end
 end
